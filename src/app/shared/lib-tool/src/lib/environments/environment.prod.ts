4578ab5454b56887a526e7a6363eaa4c3d42672c
Json Jimenez <jeison@jeison.com>
2023-06-20 22:22:08 -0500

    update

--- src/app/shared/lib-tool/src/lib/environments/environment.prod.ts
+++ src/app/shared/lib-tool/src/lib/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://dev-emisoras.streamingsonidointerno.com/api'
+  apiUrl: 'https://dev-emisoras.streamingsonidointerno.com/api/v1'
 };
