d8f7c0a9c503c1e6f5a9308512120199720a3e86
jsonjimenez <jeisonjimenes.1998@gmail.com>
2024-02-13 14:41:35 -0500

    deploy fix new get

--- src/app/shared/components/without-internet/without-internet.component.ts
+++ src/app/shared/components/without-internet/without-internet.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-without-internet',
@@ -15,11 +16,19 @@ import { MatIconModule } from '@angular/material/icon';
 })
 export class WithoutInternetComponent {
   constructor(
-    public dialogRef: MatDialogRef<WithoutInternetComponent>) {}
+    public dialogRef: MatDialogRef<WithoutInternetComponent>,
+    private router: Router
+    ) {}
 
   @Input() pos: number = 1;
 
   onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  exitUser() {
+    localStorage.clear();
+    this.router.navigateByUrl('/login');
     this.dialogRef.close();
   }
 }
