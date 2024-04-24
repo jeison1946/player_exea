bf86f4afbf63eca64e61c113ca2e1c109ae683ed
Json Jimenez <jeisonjimenes.1998@gmail.com>
2024-04-24 11:16:04 -0500

    update fix new

--- src/app/shared/lib-tool/src/lib/environments/environment.prod_default.ts
+++ src/app/shared/lib-tool/src/lib/environments/environment.prod_default.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://emisoras2.exeamedia.com/api/v1',
-  apiNode: 'https://api-dev-epa.us.seedcloud.co'
+  apiNode: 'https://api-emisoras2.exeamedia.com'
 };
